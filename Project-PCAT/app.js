8308e11f89d35e5900bd77d7f8f5858e0a9e0703
ztrkosmn09 <ztrkosmn09@gmail.com>
2022-11-06 12:01:20 +0100

    ejs wurde hinzufügen

--- Project-PCAT/app.js
+++ Project-PCAT/app.js
@@ -1,28 +1,33 @@
-import express from'express';
-import path from'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const app =express();
-const port =3001;
-const photo = {
-        id:1,
-        name:"Photo Name",
-        description:"Photo description"
-}
+import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
 
+const port = 3001;
+const photo = {
+  id: 1,
+  name: 'Photo Name',
+  description: 'Photo description',
+};
+const app = express();
+
+// Template engine
+
+app.set('view engine', 'ejs');
 
 // MIDDLEWARE
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
 
-app.get('/',(req,res)=>{
-     res.sendFile(path.resolve(__dirname,'temp/index.html')) 
-})
-
-app.listen(port ,()=>{
-        console.log(`Server wurde http://localhost:${port} aufgeführt `);
-})
-;
+app.listen(port, () => {
+  console.log(`Server wurde http://localhost:${port} aufgeführt `);
+});
