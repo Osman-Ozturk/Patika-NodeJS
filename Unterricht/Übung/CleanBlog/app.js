1416e78e83fdce494a7108cbc6df4d75dacc2a87
ztrkosmn09 <ztrkosmn09@gmail.com>
2022-11-06 12:35:09 +0100

    task8 wurde erlegit

--- Unterricht/Übung/CleanBlog/app.js
+++ Unterricht/Übung/CleanBlog/app.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const ejs = require('ejs')
 const app = express();
 
-app.get('/',(req,res)=>{
-        const blog = { id: 1, title: "Blog title", description: "Blog description" }  
+app.set('view engine','ejs')
+app.use(express.static('public'))
 
-        res.send(blog)
+
+app.get('/',(req,res)=>{
+        res.render('index')
 })
+app.get('/about',(req,res)=>{
+        res.render('about')
+})
+app.get('/add_post',(req,res)=>{
+        res.render('add_post')
+})
+
+
+
 app.listen(3001,()=>{
         console.log(`http://localhost:${3001}`);
 })
