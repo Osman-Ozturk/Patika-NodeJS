84a0edc93178fbe55206231ab0f57197f6a7e354
ztrkosmn09 <ztrkosmn09@gmail.com>
2022-11-07 00:04:39 +0100

    Task 9 wurde erledigt

--- Unterricht/Übung/CleanBlog/app.js
+++ Unterricht/Übung/CleanBlog/app.js
@@ -1,23 +1,34 @@
-const express = require('express')
-const ejs = require('ejs')
+import express from "express";
+import ejs from "ejs";
+import mongoose from "mongoose";
+import  Post  from "./modules/Post.js";
 const app = express();
 
-app.set('view engine','ejs')
-app.use(express.static('public'))
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
 
 
-app.get('/',(req,res)=>{
-        res.render('index')
-})
-app.get('/about',(req,res)=>{
-        res.render('about')
-})
-app.get('/add_post',(req,res)=>{
-        res.render('add_post')
-})
+// middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.get("/", async (req, res) => {
+  const posts =await Post.find();
+  res.render("index", { posts });
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
+});
 
-
-app.listen(3001,()=>{
-        console.log(`http://localhost:${3001}`);
-})
+const port = 4000;
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
